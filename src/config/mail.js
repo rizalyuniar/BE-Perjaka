422841481a2d03e5e801cdc41914f886b2dd8c8c
unknown <rizalyuniar123@gmail.com>
2023-07-21 21:34:02 +0700

    email

--- src/config/mail.js
+++ src/config/mail.js
@@ -8,14 +8,14 @@ function sendMail(token, email) {
         port: 587,
         secure: false,
         auth: {
-            user: process.env.EMAIL_SENDER, // generated ethereal user
-            pass: process.env.EMAIL_SENDER_PASSWORD, // generated ethereal password
+            user: "rizalyuniar012@gmail.com", // generated ethereal user
+            pass: "meicbiyqaqcgkhrp", // generated ethereal password
         },
     });
 
     // Content
     configMail.sendMail({
-        from: process.env.EMAIL_SENDER, // sender address
+        from: "rizalyuniar012@gmail.com", // sender address
         to: email, // list of receivers
         subject: "Perjaka Activation Link", // Subject line
         html: `<b>https://si-perjaka.vercel.app/verification?token=${token}</b>`, // html body
