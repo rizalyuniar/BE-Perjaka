bf5e6154efc94fd118368ace5988570d956dc702
unknown <rizalyuniar123@gmail.com>
2023-07-22 14:19:30 +0700

    nodemailer

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,25 +1,31 @@
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 
-function sendMail(token, email) {
+async function sendMail(token, email) {
     // Config
     const configMail = nodemailer.createTransport({
         host: "smtp.gmail.com",
         port: 587,
         secure: false,
         auth: {
-            user: "rizalyuniar012@gmail.com", // generated ethereal user
-            pass: "meicbiyqaqcgkhrp", // generated ethereal password
+            user: process.env.EMAIL_SENDER, // generated ethereal user
+            pass: process.env.EMAIL_SENDER_PASSWORD, // generated ethereal password
         },
     });
 
+    try {
+        await configMail.sendMail({
+            from: process.env.EMAIL_SENDER, // sender address
+            to: email, // list of receivers
+            subject: "Perjaka Activation Link", // Subject line
+            html: `<b>https://si-perjaka.vercel.app/verification?token=${token}</b>`, // html body
+        });
+        console.log("Email terkirim dengan sukses!");
+    } catch (error) {
+        console.error("Gagal mengirim email:", error);
+    }
     // Content
-    configMail.sendMail({
-        from: "rizalyuniar012@gmail.com", // sender address
-        to: email, // list of receivers
-        subject: "Perjaka Activation Link", // Subject line
-        html: `<b>https://si-perjaka.vercel.app/verification?token=${token}</b>`, // html body
-    });
+    
     return;
 }
 
